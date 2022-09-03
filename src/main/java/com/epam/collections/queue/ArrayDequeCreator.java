87e5936c7fde8d3f3db186cc87e274f9806f4358
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-09-04 01:41:22 +0600

    do

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,12 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.addLast(result.pollFirst());
+        result.addLast(result.pollFirst());
+        while (firstQueue.isEmpty()) {
+            firstQueue.add(result.pollFirst());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        ArrayDeque<Integer> dishes = new ArrayDeque<>();
+        for (int i = 1; i < numberOfDishes; i++) {
+            dishes.add(i);
+        }
+        List<Integer> result = new ArrayList<>(numberOfDishes);
+        int k = 1;
+        while (!dishes.isEmpty()) {
+            if (k == 3) {
+                result.add(dishes.pollFirst());
+            }
+            if (k == 4) {
+                k = 0;
+            }
+            k++;
+        }
+        return result;
     }
 }
